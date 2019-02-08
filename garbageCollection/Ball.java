540b33aeed28ab720dca0f452000c46ddc208d6e
Stephen <stephen@umw.edu>
2019-02-08 09:26:07 -0500

    Add Ball.java from chapter 8.

---
+++ garbageCollection/Ball.java
@@ -0,0 +1,33 @@
+
+class Ball {
+    private String color;
+    private int airPressure;
+
+    Ball(String color) {
+        this.color = color;
+        airPressure = 0;
+    }
+
+    void bounce() {
+        System.out.println("Boing!!");
+    }
+
+    static Ball play(int numBalls) {
+        ArrayList equipment = new ArrayList();
+        Ball b;
+        int i;
+        for (i=0; i<numBalls; i++) {
+            b = new Ball("red");
+            b.bounce();
+            equipment.add(b);
+        }
+        Ball basket = new Ball("orange");
+        return basket;
+    }
+
+    public static void main(String args[]) {
+        int x = 3;
+        Ball myBall = play(2);
+        System.out.println("My ball is " + myBall.color + ".");
+    }
+}
